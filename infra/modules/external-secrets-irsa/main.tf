17e8d55096b4f402d3a4941ddfd8fd6e0c936637
karty11 <158238682+karty11@users.noreply.github.com>
2025-09-03 21:30:44 +0530

    Update main.tf

--- infra/modules/external-secrets-irsa/main.tf
+++ infra/modules/external-secrets-irsa/main.tf
@@ -67,25 +67,43 @@ resource "aws_iam_role_policy_attachment" "external_secrets" {
 }
 
 # Create namespace for External Secrets
-data "kubernetes_namespace" "external_secrets" {
+#data "kubernetes_namespace" "external_secrets" {
+  #metadata {
+    #name = var.namespace
+  }
+}
+
+# Annotate the service account with IRSA role
+#resource "kubernetes_service_account" "external_secrets" {
+  #metadata {
+    #name      = var.service_account_name
+    #namespace = data.kubernetes_namespace.external_secrets.metadata[0].name
+    #annotations = {
+     # "eks.amazonaws.com/role-arn" = aws_iam_role.external_secrets.arn
+    }
+  }
+}
+
+# Create namespace for External Secrets
+resource "kubernetes_namespace" "external_secrets" {
   metadata {
     name = var.namespace
   }
 }
 
-# Annotate the service account with IRSA role
 resource "kubernetes_service_account" "external_secrets" {
   metadata {
     name      = var.service_account_name
-    namespace = data.kubernetes_namespace.external_secrets.metadata[0].name
+    namespace = kubernetes_namespace.external_secrets.metadata[0].name
     annotations = {
       "eks.amazonaws.com/role-arn" = aws_iam_role.external_secrets.arn
     }
   }
 }
 
+
 resource "null_resource" "check_cluster" {
   provisioner "local-exec" {
-    command = "echo '✅ External Secrets IRSA deployed for cluster ${var.cluster_name}, namespace ${var.namespace}'"
+    command = "echo 'External Secrets IRSA deployed for cluster ${var.cluster_name}, namespace ${var.namespace}'"
   }
 }
